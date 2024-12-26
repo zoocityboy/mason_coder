b0c94e793e36be98125821689b55f633b11af5e0
zoocityboy <zoocityboy@gmail.com>
2024-12-26 23:51:08 +0100

    feat(config): update site and base URL in astro.config.mjs; add npm ci script to package.json

--- docs/astro.config.mjs
+++ docs/astro.config.mjs
@@ -4,6 +4,8 @@ import starlight from '@astrojs/starlight';
 
 // https://astro.build/config
 export default defineConfig({
+	site: "https://zoocityboy.github.io/",
+	base: "/mason_coder/",
 	integrations: [
 		starlight({
 			title: 'Mason Coder',
