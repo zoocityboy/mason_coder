7b01bf8061cd4d6f19f2d643c916c276face8f58
zoocityboy <zoocityboy@gmail.com>
2024-12-27 01:23:29 +0100

    feat(docs): integrate Tailwind CSS and update documentation structure; add new guides for usage, installation, and configuration

--- docs/astro.config.mjs
+++ docs/astro.config.mjs
@@ -1,30 +1,53 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
-import starlight from '@astrojs/starlight';
+import { defineConfig } from "astro/config";
+import starlight from "@astrojs/starlight";
+import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-	site: "https://zoocityboy.github.io/",
-	base: "/mason_coder/",
-	integrations: [
-		starlight({
-			title: 'Mason Coder',
-			social: {
-				github: 'https://github.com/zoocityboy/mason_coder',
-			},
-			sidebar: [
-				{
-					label: 'Guides',
-					items: [
-						// Each item here is one entry in the navigation menu.
-						{ label: 'Example Guide', slug: 'guides/example' },
-					],
-				},
-				{
-					label: 'Reference',
-					autogenerate: { directory: 'reference' },
-				},
-			],
-		}),
-	],
+  site: "https://zoocityboy.github.io/",
+  base: "/mason_coder/",
+  trailingSlash: "never",
+  integrations: [
+    starlight({
+      title: "Mason Coder",
+      social: {
+        github: "https://github.com/zoocityboy/mason_coder",
+      },
+      sidebar: [
+        // { label: 'Home', link: '/' },
+        {
+          label: "Getting Started",
+          collapsed: false,
+          // items: [
+          //   // Each item here is one entry in the navigation menu.
+          //   { label: "Intro", slug: "guides/intro" },
+          //   { label: "Installation", slug: "guides/installation" },
+          //   { label: "Usage", slug: "guides/usage" },
+          //   { label: "Example Guide", slug: "guides/example" },
+          // ],
+          autogenerate: { directory: "start" },
+        },
+        {
+          label: "Create Bricks",
+          collapsed: true,
+          // items: [
+          //   // Each item here is one entry in the navigation menu.
+          //   { label: "Intro", slug: "guides/intro" },
+          //   { label: "Installation", slug: "guides/installation" },
+          //   { label: "Usage", slug: "guides/usage" },
+          //   { label: "Example Guide", slug: "guides/example" },
+          // ],
+          autogenerate: { directory: "create" },
+        },
+        {
+          label: "Reference",
+          collapsed: true,
+          autogenerate: { directory: "reference" },
+        },
+      ],
+      customCss: ["./src/tailwind.css"],
+    }),
+    tailwind({ applyBaseStyles: false }),
+  ],
 });
